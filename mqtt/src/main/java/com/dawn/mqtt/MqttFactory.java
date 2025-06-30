1ef850ac8d5c3fbddf89bec3bfccf43dd90c3097
dawnbai <904494377@qq.com>
2025-06-30 17:42:33 +0800

    修改心跳时间
    修改心跳时间

--- mqtt/src/main/java/com/dawn/mqtt/MqttFactory.java
+++ mqtt/src/main/java/com/dawn/mqtt/MqttFactory.java
@@ -95,6 +95,59 @@ public class MqttFactory {
         }
     }
 
+    public void init(String serverUri, String clientId, String username, String password, String topic, String topicService, String onlineCommand, String offlineCommand, String reconnectCommand, MqttListener listener, int timeout) {
+        try {
+            this.topic = topic;
+            this.reconnectCommand = reconnectCommand;
+            mqttClient = new MqttClient(serverUri, clientId, new MemoryPersistence());
+            mqttConnectOptions = new MqttConnectOptions();
+            mqttConnectOptions.setUserName(username);
+            mqttConnectOptions.setPassword(password.toCharArray());
+            //设置遗嘱消息
+            //qos为0：“至多一次”，消息发布完全依赖底层 TCP/IP 网络。会发生消息丢失或重复。这一级别可用于如下情况，环境传感器数据，丢失一次读记录无所谓，因为不久后还会有第二次发送。
+            //qos为1：“至少一次”，确保消息到达，但消息重复可能会发生。这一级别可用于如下情况，你需要获得每一条消息，并且消息重复发送对你的使用场景无影响。
+            //qos为2：“只有一次”，确保消息到达一次。这一级别可用于如下情况，在计费系统中，消息重复或丢失会导致不正确的结果。
+            if(!topic.isEmpty() || !offlineCommand.isEmpty())
+                mqttConnectOptions.setWill(topic, offlineCommand.getBytes(), 2, true);
+            ///设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录，这里设置为true表示每次连接到服务器都以新的身份连接
+            mqttConnectOptions.setCleanSession(false);
+            mqttConnectOptions.setConnectionTimeout(10);
+            //设置好心跳后如果客户端在1.5个心跳时间没有发送心跳包（16位的字）服务器就断定和客户端失去连接。
+            mqttConnectOptions.setKeepAliveInterval(timeout);
+            mqttClient.connect(mqttConnectOptions);
+            subscribeToTopic(topicService);
+            if(!onlineCommand.isEmpty())
+                publishMessage(topic, onlineCommand);
+
+            mqttClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    //断开连接，重连
+                    connected = false;
+                    handler.post(runnable);
+                    if(listener != null)
+                        listener.onConnectFailure();
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    if(listener != null)
+                        listener.onMessageArrived(topic, message.toString());
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    connected = true;
+                    if(listener != null)
+                        listener.onConnectSuccess();
+                }
+            });
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * 订阅主题
      * @param topic 主题
